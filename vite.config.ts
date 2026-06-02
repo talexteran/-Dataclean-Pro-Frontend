4c5adf1573ee31699977872fe8c4128d3a4c3e6e
talexteran <talexteran@gmail.com>
2026-06-01 23:35:14 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/-Dataclean-Pro/',
+  base: process.env.NODE_ENV === 'production'
+    ? '/-Dataclean-Pro/'
+    : '/'
 })
